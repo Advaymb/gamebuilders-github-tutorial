4f18a323649650e57baaf5a17d4a15707a6df323
Advaymb <57421090+Advaymb@users.noreply.github.com>
2021-10-25 20:54:45 -0500

    Skymat added, DestroyonClick modified
    incomplete, finish later

--- Assets/Scripts/DestroyonClick.cs
+++ Assets/Scripts/DestroyonClick.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class DestroyonClick : MonoBehaviour
 {
-    private void OnMouseDown()
+    private void Update()
     {
-        Destroy(gameObject);
-        Debug.Log("Time survived: " + Time.time + "s");
+        if(Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.name == "Player")
+                {
+                    Destroy(gameObject);
+                    Debug.Log("Time survived: " + Time.time + "s");
+                }
+            }
+        }
+        
     }
 }
